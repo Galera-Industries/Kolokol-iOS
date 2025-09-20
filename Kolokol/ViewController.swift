fe07f4560f5df6a0325bee45f7ef21c46f4fdbe4
Арсений Потякин <arseniy@MacBook-Pro-Arsenij.local>
2025-09-20 14:55:30 +0300

    Project init

---
+++ Kolokol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kolokol
+//
+//  Created by Арсений Потякин on 20.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
